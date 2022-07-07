aae1843fba9143814582ad79dab6c4e6f05d4985
Carlos Sousa <carlos.sousa@youxgroup.com.br>
2022-07-07 16:40:53 -0300

    Esse é meu primeiro commit

---
+++ Usuarios/src/test/java/com/api/usuarios/Usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.usuarios.Usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
